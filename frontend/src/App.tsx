fbd798f0d4e436c68222e7b7e7086436ff66e8fc
soner3 <sonerastan@icloud.com>
2024-07-16 19:10:13 +0200

    Added Alert

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import Main from "./components/Main/Main";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
+  alert(
+    "No Real Shopping Site! I just hosted on of my React Projects as a placeholder for this Domain."
+  );
   return (
     <BrowserRouter>
       <Header />
